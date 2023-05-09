898f47bb1033b90e84bafd44117d4edc28fe7bc0
Jon Church <me@jonchurch.com>
2023-05-09 00:32:55 -0400

    format date Countdown as human friendly string

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,29 +1,23 @@
 import { useEffect, useState } from 'react';
-import differenceInSeconds from 'date-fns/differenceInSeconds';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 type CountdownProps = {targetDate: Date}
 
 export const Countdown = ({ targetDate }: CountdownProps) => {
-  const [timeLeft, setTimeLeft] = useState(differenceInSeconds(new Date(targetDate), new Date()));
+  const [timeLeft, setTimeLeft] = useState<string>(formatDistanceToNow(targetDate))
 
   useEffect(() => {
     // Update the time left every second
     const timer = setInterval(() => {
-      setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+      setTimeLeft(formatDistanceToNow(new Date(targetDate)));
     }, 1000);
 
     // Clear the interval when the component unmounts
     return () => clearInterval(timer);
   }, []);
 
-  // Calculate the days, hours, minutes, and seconds
-  const days = Math.floor(timeLeft / (60 * 60 * 24));
-  const hours = Math.floor((timeLeft / (60 * 60)) % 24);
-  const minutes = Math.floor((timeLeft / 60) % 60);
-  const seconds = timeLeft % 60;
-
   return (
-    <span>{days}d {hours}h {minutes}m {seconds}s left</span>
+    <span>{timeLeft}</span>
   );
 };
 
