e99a397f4cd94936f668437d314e5798708483d0
Jon Church <me@jonchurch.com>
2023-05-09 00:07:59 -0400

    move ContactCard to its own component

---
+++ components/ContractCard.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react'
+import format from 'date-fns/format';
+import { Contract } from '@/packages/space-sdk';
+
+export const ContractCard = ({ contract }: {contract: Contract}) => {
+    return (
+        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+            <h2 className="text-xl font-bold mb-2">{contract.factionSymbol}</h2>
+            <p className="text-sm mb-2">Type: {contract.type}</p>
+            <p className="text-sm mb-2">Deadline: {format(new Date(contract.terms.deadline), 'dd/MM/yyyy hh:mm')}</p>
+            <p className="text-sm mb-2">Payment on acceptance: {contract.terms.payment.onAccepted}</p>
+            <p className="text-sm mb-2">Payment on fulfillment: {contract.terms.payment.onFulfilled}</p>
+            {contract.terms.deliver?.map((item, index) => (
+                <div key={index} className="mb-2">
+                    <p className="text-sm">Trade Symbol: {item.tradeSymbol}</p>
+                    <p className="text-sm">Destination: {item.destinationSymbol}</p>
+                    <p className="text-sm">Units required: {item.unitsRequired}</p>
+                    <p className="text-sm">Units fulfilled: {item.unitsFulfilled}</p>
+                </div>
+            ))}
+            <p className="text-sm mb-2">Accepted: {contract.accepted ? 'Yes' : 'No'}</p>
+            <p className="text-sm mb-2">Fulfilled: {contract.fulfilled ? 'Yes' : 'No'}</p>
+            <p className="text-sm mb-2">Expiration: {format(new Date(contract.expiration), 'dd/MM/yyyy hh:mm')}</p>
+        </div>
+    );
+};
+
