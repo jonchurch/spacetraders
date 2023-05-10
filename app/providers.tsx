1faa15ee47fe8b8b8d35c9cda02eec8fc698f78b
Jon Church <me@jonchurch.com>
2023-05-09 23:27:32 -0400

    setup ClientQuery provider in layout

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+export default function Providers({ children }: {children: ReactNode}) {
+  const [queryClient] = React.useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
