9719ccde7d390f3f08b98a066ba3cc4dca1d2c8e
Jon Church <me@jonchurch.com>
2023-05-09 22:53:52 -0400

    submit to rtk query's whims and use fetch

--- redux/spacetraders.ts
+++ redux/spacetraders.ts
@@ -1,7 +1,6 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { FleetApi, Ship } from '@/packages/space-sdk'
-import { config } from '../api'
+import { Ship } from '@/packages/space-sdk'
 
 // Define a service using a base URL and expected endpoints
 export const spacetradersApi = createApi({
@@ -9,15 +8,7 @@ export const spacetradersApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.spacetraders.io/v2/' }),
   endpoints: (builder) => ({
      getShips: builder.query<Ship[], void>({
-      queryFn: async () => {
-        const fleet = new FleetApi(config)
-        try {
-          const { data } = await fleet.getMyShips()
-          return { data }
-        } catch (error) {
-          return {error: {message: 'oops'}}
-        }
-      }
+      query: () => '/my/ships'
     }),
   }),
 })
