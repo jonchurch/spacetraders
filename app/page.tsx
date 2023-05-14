ac7b2e28d6c14b6c783996a83d262bfac2c20634
Jon Church <me@jonchurch.com>
2023-05-14 12:54:36 -0400

    comment out MakeError button

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
       <AppShell />
       {/* <main className="flex min-h-screen flex-col items-center justify-between "> */}
       <main>
-          <div className="flex flex-col items-center justify-between min-h-screen mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <MakeError />
+        <div className="flex flex-col items-center justify-between min-h-screen mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* <MakeError /> */}
           <Ships />
           <Asteroids waypoints={waypointData ?? []}/>
           <Contracts />
