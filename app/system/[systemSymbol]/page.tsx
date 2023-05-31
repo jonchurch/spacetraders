92949e4a03dad1fa5515962be4295b242f3aed1d
Jon Church <me@jonchurch.com>
2023-05-31 16:28:03 -0400

    read system data statically, ignore files in data dir

--- app/system/[systemSymbol]/page.tsx
+++ app/system/[systemSymbol]/page.tsx
@@ -1,7 +1,4 @@
 'use client'
-import { SystemInfo } from '@/components/SystemInfo'
-import { useQuery } from '@tanstack/react-query'
-import {  getSystemWaypoints } from '@/api'
 import AppShell from '@/components/AppShell'
 
 type SystemParams = {
@@ -10,17 +7,15 @@ type SystemParams = {
 
 export default function SystemPage({params}: {params: SystemParams} ) {
   const { systemSymbol } = params
-    const { data: waypointData } = useQuery({
-        queryKey: ['systemWaypoints', systemSymbol],
-        queryFn: () => getSystemWaypoints(systemSymbol)
-    })
   return (
     <>
       <AppShell />
       {/* <main className="flex min-h-screen flex-col items-center justify-between "> */}
       <main>
         <div className="flex flex-col items-center justify-between min-h-screen mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <SystemInfo systemSymbol={systemSymbol}/>
+        {/* <Asteroids waypoints={waypointData} /> */}
+        <SystemCard system={systemData}/>
+        {/* <Waypoints waypointsData={waypointData}/> */}
         </div>
       </main>
     </>
