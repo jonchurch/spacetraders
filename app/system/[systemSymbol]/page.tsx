abf64349c11b1d9ac67bba825e6b784227f579fe
Jon Church <me@jonchurch.com>
2023-05-31 13:09:17 -0400

    add prettierrc

---
+++ app/system/[systemSymbol]/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { Asteroids, SystemInfo } from '@/components/SystemInfo'
+import Contracts from '@/components/Contracts'
+import { useQuery } from '@tanstack/react-query'
+import { getSystemAndWaypoints } from '@/api'
+import AppShell from '@/components/AppShell'
+
+export default function Home() {
+  const systemSymbol = 'X1-DC54'
+    const { data } = useQuery({
+        queryKey: ['systemAndWaypoints', systemSymbol],
+        queryFn: () => getSystemAndWaypoints(systemSymbol)
+    })
+    const {systemData: _, waypointData} = data ?? {}
+  return (
+    <>
+      <AppShell />
+      {/* <main className="flex min-h-screen flex-col items-center justify-between "> */}
+      <main>
+        <div className="flex flex-col items-center justify-between min-h-screen mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <SystemInfo systemSymbol={systemSymbol}/>
+        </div>
+      </main>
+    </>
+  )
+}
