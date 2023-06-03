5709335cbd60d3358ad0f837c4d541cb6269c35b
Jon Church <me@jonchurch.com>
2023-06-03 13:58:26 -0400

    comment out nav header that is unused

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -77,21 +77,21 @@ export default function AppShell() {
                     </div>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? 'bg-gray-800 text-white'
-                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                              'rounded-md px-3 py-2 text-sm font-medium'
-                            )}
-                            aria-current={item.current ? 'page' : undefined}
-                          >
-                            {item.name}
-                          </a>
-                        ))}
+                        {/* {navigation.map((item) => ( */}
+                        {/*   <a */}
+                        {/*     key={item.name} */}
+                        {/*     href={item.href} */}
+                        {/*     className={classNames( */}
+                        {/*       item.current */}
+                        {/*         ? 'bg-gray-800 text-white' */}
+                        {/*         : 'text-gray-300 hover:bg-gray-700 hover:text-white', */}
+                        {/*       'rounded-md px-3 py-2 text-sm font-medium' */}
+                        {/*     )} */}
+                        {/*     aria-current={item.current ? 'page' : undefined} */}
+                        {/*   > */}
+                        {/*     {item.name} */}
+                        {/*   </a> */}
+                        {/* ))} */}
                       </div>
                     </div>
                   </div>
